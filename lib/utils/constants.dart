7945691bae3860b9ac97691c89535bdce1643f74
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-05-21 14:56:45 +1000

    remove unused code

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,24 +1,3 @@
-import 'package:flutter/material.dart';
-
 //API
 const apiKey = 'c09c68c7b21b2f7af2cd4b124f988b75';
 const baseUrl = 'https://api.darksky.net/forecast';
-
-const kTempTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 100.0,
-);
-
-const kMessageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 60.0,
-);
-
-const kButtonTextStyle = TextStyle(
-  fontSize: 30.0,
-  fontFamily: 'Spartan MB',
-);
-
-const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
-);
